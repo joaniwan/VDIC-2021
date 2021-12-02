a06d03eb3fdaac19d6dd85cedac376ab12a63f8d
joaniwan <joan.iwan@gmail.com>
2021-12-02 15:06:55 +0100

    Minor updates

--- lab05/tb_classes/minmax_tester.svh
+++ lab05/tb_classes/minmax_tester.svh
@@ -5,17 +5,6 @@ class minmax_tester extends random_tester;
 	function new (string name, uvm_component parent);
         super.new(name, parent);
     endfunction : new
-
-	function byte get_data();
-	    bit zero_ones;
-	    zero_ones = 1'($random);
-	    if (zero_ones == 1'b0)
-	        return 8'h00;
-	    else if (zero_ones == 1'b1)
-	        return 8'hFF;
-	    else
-			return 8'($random);
-	endfunction : get_data
 	
 	function [98:0] get_packet(input [2:0] op_set, input [2:0] expected_error);  //get packet with data 
 		bit [10:0] Data1,Data2,Data3,Data4,Data5,Data6,Data7,Data8,Data9;
@@ -65,31 +54,31 @@ class minmax_tester extends random_tester;
 		        Data8[8:1] =8'b0; 
 		        Data9      = get_ctl_packet({Data1[8:1], Data2[8:1], Data3[8:1], Data4[8:1]}, {Data5[8:1], Data6[8:1], Data7[8:1], Data8[8:1]}, op_set);	        
 	            end
-	        default:begin 
-				zero_ones = 1'($random);
-			    if (zero_ones == 1'b0) begin
-			    	Data1[8:1] =8'b0; 
-			        Data2[8:1] =8'b0; 
-			        Data3[8:1] =8'b0; 
-			        Data4[8:1] =8'b0; 
-			        Data5[8:1] =8'b0; 
-			        Data6[8:1] =8'b0; 
-			        Data7[8:1] =8'b0; 
-			        Data8[8:1] =8'b0; 
-				    end
-			    else begin
-		        	Data1[8:1] =8'hFF; 
-			        Data2[8:1] =8'hFF;  
-			        Data3[8:1] =8'hFF;  
-			        Data4[8:1] =8'hFF;  
-			        Data5[8:1] =8'hFF;  
-			        Data6[8:1] =8'hFF;  
-			        Data7[8:1] =8'hFF;  
-			        Data8[8:1] =8'hFF; 	  
-			    end
-			 Data9      = get_ctl_packet({Data1[8:1], Data2[8:1], Data3[8:1], Data4[8:1]}, {Data5[8:1], Data6[8:1], Data7[8:1], Data8[8:1]}, op_set);			
+	        default:begin 				
 	        end	        
-		endcase		
+		endcase	
+		zero_ones = 1'($random);
+	    if (zero_ones == 1'b0) begin
+	    	Data1[8:1] =8'b0; 
+	        Data2[8:1] =8'b0; 
+	        Data3[8:1] =8'b0; 
+	        Data4[8:1] =8'b0; 
+	        Data5[8:1] =8'b0; 
+	        Data6[8:1] =8'b0; 
+	        Data7[8:1] =8'b0; 
+	        Data8[8:1] =8'b0; 
+		    end
+	    else begin
+        	Data1[8:1] =8'hFF; 
+	        Data2[8:1] =8'hFF;  
+	        Data3[8:1] =8'hFF;  
+	        Data4[8:1] =8'hFF;  
+	        Data5[8:1] =8'hFF;  
+	        Data6[8:1] =8'hFF;  
+	        Data7[8:1] =8'hFF;  
+	        Data8[8:1] =8'hFF; 	  
+	    end
+	    Data9      = get_ctl_packet({Data1[8:1], Data2[8:1], Data3[8:1], Data4[8:1]}, {Data5[8:1], Data6[8:1], Data7[8:1], Data8[8:1]}, op_set);				    
 	    return   {Data1, Data2, Data3, Data4, Data5, Data6, Data7, Data8, Data9};   
 	endfunction : get_packet
 endclass : minmax_tester
